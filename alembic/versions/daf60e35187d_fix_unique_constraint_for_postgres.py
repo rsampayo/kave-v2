63f60dbbdde80436d310263fa1431cbc14eb8757
Ramon Sampayo <ramon@ukko.mx>
2025-04-16 09:21:29 -0500

    Fix multiple heads in Alembic migrations

--- alembic/versions/daf60e35187d_fix_unique_constraint_for_postgres.py
+++ alembic/versions/daf60e35187d_fix_unique_constraint_for_postgres.py
@@ -1,7 +1,7 @@
 """fix_unique_constraint_for_postgres
 
 Revision ID: daf60e35187d
-Revises: 7b40d1074b24
+Revises: 443a8e83492a
 Create Date: 2025-04-16 09:19:43.413577
 
 """
@@ -14,7 +14,7 @@ from sqlalchemy.exc import OperationalError
 
 # revision identifiers, used by Alembic.
 revision: str = 'daf60e35187d'
-down_revision: Union[str, None] = '7b40d1074b24'
+down_revision: Union[str, None] = '443a8e83492a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
